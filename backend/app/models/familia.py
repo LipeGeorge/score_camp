fb9f8d94bf9bd0138d82d2da18ecf8c7d5f00a49
George Felipe <georgelipe2003@gmail.com>
2026-05-21 15:58:51 -0300

    conclusao models familia, participante e prova

--- backend/app/models/familia.py
+++ backend/app/models/familia.py
@@ -1,3 +1,17 @@
 from sqlmodel import SQLModel, Field, Relationship
 from typing import List
+from .participante import Participante
 
+
+class Familia(SQLModel, table=True):
+    
+    id: int = Field(default=None, primary_key=True)
+    
+    nome: str = Field(default=None, max_length=100, unique=True, 
+                                description="Nome da família")
+    
+    cor: str = Field(
+        default=None, max_length=7, nullable=False, 
+        description="Valor hexadecimal da cor da familia")
+    
+    participantes: List["Participante"] = Relationship(back_populates="familia")
--- backend/app/models/prova.py
+++ backend/app/models/prova.py
@@ -1,2 +1,10 @@
-from sqlmodel import SQLModel, Field, Relationship
-from typing import List
+from sqlmodel import SQLModel, Field
+
+class Prova:
+    
+    id: int = Field(default=None, primary_key=True)
+    
+    nome: str = Field(default=None, max_length=150, unique=True, 
+                                description="Nome da prova")
+    
+    teto: int = Field(default=None, description="pontuacao maxima que a prova pode dar")
