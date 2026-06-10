a938711753a152caf2ea7e1ea8c07eaeafb24082
George Felipe <georgelipe2003@gmail.com>
2026-06-10 11:10:53 -0300

    feat(db): criacao das tabelas no banco

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,22 +1,8 @@
 from fastapi import FastAPI
-from .database.database import engine
-from sqlmodel import SQLModel
-
-from app.models.familia import Familia
-from app.models.inscrito import Inscrito
-from app.models.prova import Prova
-
-
 from .routes.inscrito_router import router as inscritos_router
 
 
-def lifespan(app: FastAPI):
-    
-    SQLModel.metadata.create_all(engine)
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 
 app.include_router(inscritos_router)
